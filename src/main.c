56419532ef55f798c1fab30d7cc8269e1e0916e5
rbaron <rbaron@rbaron.net>
2025-05-06 22:55:45 +0200

    Example BLE

--- src/main.c
+++ src/main.c
@@ -21,6 +21,28 @@
 //     DEVICE_DT_GET(DT_NODELABEL(npm1300_regulators));
 // static const struct device *ldo2 = DEVICE_DT_GET(DT_NODELABEL(npm1300_ldo2));
 
+// BLE.
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+    BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
+    BT_DATA_BYTES(BT_DATA_SVC_DATA16, 0xaa, 0xfe, /* Eddystone UUID */
+                  0x10,                           /* Eddystone-URL frame type */
+                  0x00, /* Calibrated Tx power at 0m */
+                  0x00, /* URL Scheme Prefix http://www. */
+                  'z', 'e', 'p', 'h', 'y', 'r', 'p', 'r', 'o', 'j', 'e', 'c',
+                  't', 0x08) /* .org */
+};
+
+/* Set Scan Response data */
+static const struct bt_data sd[] = {
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
 static const struct device *leds = DEVICE_DT_GET(DT_NODELABEL(npm1300_leds));
 
 LOG_MODULE_REGISTER(main, CONFIG_DISPLAY_LOG_LEVEL);
@@ -53,6 +75,38 @@ static struct gpio_callback button_cb_data;
 static void button_pressed(const struct device *dev, struct gpio_callback *cb,
                            uint32_t pins) {
   LOG_INF("Button pressed: pins %d", pins);
+}
+
+static void bt_ready(int err) {
+  char addr_s[BT_ADDR_LE_STR_LEN];
+  bt_addr_le_t addr = {0};
+  size_t count = 1;
+
+  if (err) {
+    LOG_ERR("Bluetooth init failed (err %d)\n", err);
+    return;
+  }
+
+  LOG_INF("Bluetooth initialized\n");
+
+  /* Start advertising */
+  err = bt_le_adv_start(BT_LE_ADV_NCONN_IDENTITY, ad, ARRAY_SIZE(ad), sd,
+                        ARRAY_SIZE(sd));
+  if (err) {
+    LOG_ERR("Advertising failed to start (err %d)\n", err);
+    return;
+  }
+
+  /* For connectable advertising you would use
+   * bt_le_oob_get_local().  For non-connectable non-identity
+   * advertising an non-resolvable private address is used;
+   * there is no API to retrieve that.
+   */
+
+  bt_id_get(&addr, &count);
+  bt_addr_le_to_str(&addr, addr_s, sizeof(addr_s));
+
+  LOG_INF("Beacon started, advertising as %s\n", addr_s);
 }
 
 int main(void) {
@@ -113,6 +167,11 @@ int main(void) {
   // if (!device_is_ready(ldo2)) {
   //   LOG_ERR("Error: LDO2 device is not ready\n");
   //   return 0;
+  // }
+
+  // ret = bt_enable(bt_ready);
+  // if (ret) {
+  //   LOG_ERR("Bluetooth init failed (err %d)\n", ret);
   // }
 
   // Power up.
